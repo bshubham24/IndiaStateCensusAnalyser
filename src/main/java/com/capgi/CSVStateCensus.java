f83c58c5f5724843ead2a9ed4b215d4c238b832d
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-20 20:17:07 +0530

    [Shubham]Welcome

---
+++ src/main/java/com/capgi/CSVStateCensus.java
@@ -0,0 +1,21 @@
+package com.capgi;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVStateCensus {
+	@CsvBindByName
+	private String state;
+	@CsvBindByName
+	private int population;
+	@CsvBindByName
+	private int AreaInSqKm;
+	@CsvBindByName
+	private int DensityPerSqKm;
+
+	@Override
+	public String toString() {
+		return "State: " + state + " " + "Population: " + population + " " + "Area: " + AreaInSqKm + " " + "Density: "
+				+ DensityPerSqKm;
+	}
+
+}
