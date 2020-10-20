ec123e51a1292fd4048a1b5453654af79793661f
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-10-20 20:54:57 +0530

    [Shubham]Added: Custom exception for incorrect csv file

---
+++ src/main/java/com/capgi/CustomCensusAnalyserException.java
@@ -0,0 +1,14 @@
+package com.capgi;
+
+public class CustomCensusAnalyserException extends Exception {
+	enum ExceptionType {
+		IncorrectCsvFile;
+	}
+
+	ExceptionType type;
+
+	public CustomCensusAnalyserException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
--- src/test/java/com/capgi/StateAnalyserCensusTest.java
+++ src/test/java/com/capgi/StateAnalyserCensusTest.java
@@ -3,17 +3,28 @@
  */
 package com.capgi;
 
+import java.io.IOException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class StateAnalyserCensusTest {
 	public static final String STATE_CENSUS_DATA_FILE = "F:\\Capgemini_training1\\java_eclipse\\IndiaStateCensusAnalyser"
 			+ "\\IndiaStateCensusData.csv";
+	public static final String WRONG_STATE_CENSUS_DATA_FILE = "F:\\Capgemini_training1\\java_eclipse\\IndiaStateCensusAnalyser"
+			+ "\\IndiaStateCensusData.cv";
 
 	@Test
-	public void whenNumberOfRecordMatchesShouldPassTest() {
+	public void whenNumberOfRecordMatchesShouldPassTest() throws CustomCensusAnalyserException, IOException {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
 		int totalEntriesInCsvFile = stateCensusAnalyser.loadCsvData(STATE_CENSUS_DATA_FILE);
+		Assert.assertEquals(29, totalEntriesInCsvFile);
+	}
+
+	@Test
+	public void whenFileIsNotInCsvFormatShouldThrowCustomException() throws CustomCensusAnalyserException, IOException {
+		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+		int totalEntriesInCsvFile = stateCensusAnalyser.loadCsvData(WRONG_STATE_CENSUS_DATA_FILE);
 		Assert.assertEquals(29, totalEntriesInCsvFile);
 	}
 }
